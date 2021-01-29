de4d04ea6450163e5db215193643ddbcaa9485cf
Finn Fiedler <finn.fiedler@smapiot.com>
2021-01-29 17:46:49 +0100

    created skeleton for piral upgrade test

--- tests/piral.test.js
+++ tests/piral.test.js
@@ -1,16 +1,17 @@
 const path = require("path");
+const fs = require('fs');
 const { toMatchFilesystemSnapshot } = require("jest-fs-snapshot");
-const { cleanDir, cleanupForSnapshot, getInitializerOptions, execute, snapshotOptions } = require("../src/common");
+const { cleanDir, cleanupForSnapshot, getInitializerOptions, execute, snapshotOptions, sleep } = require("../src/common");
 
 const cliVersion = process.env.CLI_VERSION || "latest";
 const bundlerPrefix = !!process.env.BUNDLER ? process.env.BUNDLER + "-" : "";
+const pathToBuildDir = path.resolve(process.cwd(), bundlerPrefix + "piral-inst");
 
 jest.setTimeout(300 * 1000); // 300 second timeout
 expect.extend({ toMatchFilesystemSnapshot });
 
 describe("piral", () => {
     it("scaffold piral", async () => {
-        const pathToBuildDir = path.resolve(process.cwd(), bundlerPrefix + "piral-inst");
 
         await cleanDir(pathToBuildDir);
 
@@ -23,4 +24,26 @@ describe("piral", () => {
         expect(info.stderr).toBe("");
         expect(pathToBuildDir).toMatchFilesystemSnapshot(undefined, snapshotOptions);
     });
+
+    it("upgrade piral", async () => {
+
+        const pathToPackageJson = path.resolve(`${pathToBuildDir}`, 'package.json');
+
+        let packageJson = require(pathToPackageJson);
+        packageJson.dependencies.piral = "0.12.0";
+        fs.writeFile(pathToPackageJson, JSON.stringify(packageJson));
+
+        const version = "0.12.4";
+
+        const info = await execute(`npx piral upgrade ${version}`, {
+            cwd: pathToBuildDir,
+        });
+
+        packageJson = require(pathToPackageJson);
+        const versionAfterUpgrade = packageJson.dependencies.piral;
+
+        expect(versionAfterUpgrade).toContain(version);
+        expect(info.stderr).toBe("");
+        expect(info.stdout).toContain('Piral instance upgraded successfully');
+    })
 });
